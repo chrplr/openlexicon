e07551f9057533bfa72ba75a7c582b1fb045659b
Jessica Bourgin <jessica.bourgin@gmail.com>
2020-06-24 09:01:56 +0200

    isolating packages and generate_pseudowords function

---
+++ apps/pseudowords_markov/www/functions/generatePseudo.R
@@ -0,0 +1,47 @@
+generate_pseudowords <- function (n, len, models, exclude=NULL, time.out=5)
+  # generate pseudowords by chaining trigrams
+  # n: number of pseudowords to return
+  # len: length (nchar) of these pseudowords
+  # models: vector of items to get trigrams from (all of the same length!)
+  # exclude: vector of items to exclude
+  # time.out = a time in seconds to stop
+{
+  if (length(models) == 0) { return (NULL) }
+  
+  trigs = list()  #  store lists of trigrams by starting position
+  for (cpos in 1:(len - 1))
+    trigs[[cpos]] <- substring(models, cpos, cpos + 2)
+  
+  pseudos <- character(n)  # will contain the generated pseudowords
+  
+  start.time <- Sys.time()
+  
+  np = 1
+  while ((np <= n) && ((Sys.time() - start.time) < time.out)) {
+    # sample a random beginning trigram
+    item <- sample(trigs[[1]], 1)
+    
+    # Build the item letter by letter by adding compatible trigrams
+    for(pos in 2:(len - 1)) {
+      # get the last 2 letters of the current item
+      lastbigram <- substr(item, pos, pos + 1)
+      
+      # Select trigrams staring in position 'pos' and which are compatibles with 'lastbigram'
+      compat <- trigs[[pos]][grep(paste(sep="" , "^", lastbigram), trigs[[pos]])]
+      if (length(compat) == 0) break  # must start again
+      
+      item <- paste(item, substr(sample(compat, 1), 3, 3), sep="")  # add the last letter of the trigram
+    }
+    
+    # keep item only if not in the 'models', 'exclude' or 'pseudos' list
+    if (!(item %in% c(models, exclude, pseudos))) {
+      pseudos[np] = item
+      np = np + 1
+    }
+  }
+  if (length(pseudos) == n) {
+    return (pseudos)
+  } else {
+    return (paste("could no find", n, "pseudowords"))
+  }
+}
---
+++ apps/pseudowords_markov/www/functions/loadPackages.R
@@ -0,0 +1,5 @@
+library(shiny)
+library(DT)
+library(dplyr)
+library(writexl)
+library(lubridate)
