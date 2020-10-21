ce4fb1f8c8be8756b843a0ea95c04758b4ef7763
Jessica Bourgin <jessica.bourgin@gmail.com>
2020-10-21 14:33:51 +0200

    beginning infra

---
+++ apps/infra/www/functions/loadPackages.R
@@ -0,0 +1,40 @@
+usePackage <- function(i){
+  if(! i %in% installed.packages()){
+    install.packages(i, dependencies = TRUE)
+  }
+  require(i, character.only = TRUE)
+}
+
+usePackageGit <- function(i, github_repo){
+  if(! i %in% installed.packages()){
+    library(devtools)
+    install_github(github_repo)
+  }
+  require(i, character.only = TRUE)
+}
+
+usePackage("lubridate")
+usePackage("writexl")
+usePackage("plyr")
+usePackage("stringr")
+#usePackage("dplyr")
+usePackage("DT")
+usePackage("shiny")
+usePackage("shinyjs")
+usePackage("shinyalert")
+usePackageGit("shinycssloaders", "jbourgin/shinycssloaders") #https://github.com/daattali/shinycssloaders/issues/22
+usePackage("rvest") # To repair encoding
+usePackage("shinyalert")
+usePackage("tippy")
+
+# usePackage("stringi")
+# usePackage("shinyBS")
+# usePackage("DT")
+# usePackage("stringr")
+# usePackage("shinyWidgets")
+# usePackage("data.table")
+# usePackage("rlist")
+# usePackage("RCurl") #To test if string is url
+# usePackage("comprehenr") # Comprehension list
+# usePackageGit("tippy", "JohnCoene/tippy")
+# usePackageGit("shinyTree", "shinyTree/shinyTree")
