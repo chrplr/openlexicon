b462f6f0d5d98836ae6225f37fc336a9428bc78a
Jessica Bourgin <jessica.bourgin@gmail.com>
2025-08-26 16:44:40 +0200

    change url openlexicon/serverTest to openlexicon

--- openlexicon.org/openlexiconApp/urls.py
+++ openlexicon.org/openlexiconApp/urls.py
@@ -3,8 +3,8 @@ from .views import *
 
 app_name = 'openlexiconApp'
 urlpatterns = [
-    path('serverTest', home, name="homeServer"),
-    path('serverTest/<str:column_list>', home, name="homeServer"),
+    path('', home, name="homeServer"),
+    path('<str:column_list>', home, name="homeServer"),
     path('import_data', import_data, name="import_data"),
     path('data/<str:column_list>', ItemListView.as_view(), name="data"),
     path('data/', ItemListView.as_view(), name="data")
