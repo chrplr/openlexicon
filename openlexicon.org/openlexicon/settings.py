0028b481b26d75c1ba78487f9564a16c9fcd8fef
Zebulon <christophe@pallier.org>
2025-09-12 09:32:48 +0000

    update homepage

--- openlexicon.org/openlexicon/settings.py
+++ openlexicon.org/openlexicon/settings.py
@@ -282,7 +282,7 @@ else:
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #LOGIN_URL = os.path.join(FORCE_SCRIPT_NAME, "accounts", "login")
-LOGIN_REDIRECT_URL = os.path.join(FORCE_SCRIPT_NAME, "home")
-LOGOUT_REDIRECT_URL = os.path.join(FORCE_SCRIPT_NAME, "home")
+LOGIN_REDIRECT_URL = os.path.join(FORCE_SCRIPT_NAME, "openlexicon")
+LOGOUT_REDIRECT_URL = os.path.join(FORCE_SCRIPT_NAME, "openlexicon")
 
 DEFAULT_DB = "Lexique3"
